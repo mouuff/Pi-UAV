77b6aa34b83d64d33842192b2a0ada9bdbf39380
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-03-29 13:18:02 +0200

    ok

--- protocol.py
+++ protocol.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 import socket
 import struct
 import time
--- server.py
+++ server.py
@@ -3,6 +3,19 @@
 import RPi.GPIO as GPIO
 from protocol import Server
 
+def ratio(x, min_x, max_x, new_min, new_max):
+	x = float(x)
+	if (x < min_x):
+		x = min_x
+	if (x > max_x):
+		x = max_x
+	old_range = max_x - min_x
+	new_range = new_max - new_min
+	if (old_range == 0):
+		old_range = 1
+	res = ((x - min_x) / old_range) * new_range + new_min
+	return (res)
+
 class Servo:
         FREQ = 100
         MAX_WIDTH = 25.0
@@ -11,13 +24,12 @@ class Servo:
         def __init__(self, pin):
                 GPIO.setup(pin, GPIO.OUT)
                 self.pwm = GPIO.PWM(pin, self.FREQ)
+                start_width = ratio(0.5, 0, 1, self.MIN_WIDTH, self.MAX_WIDTH)
                 self.pwm.start(0)
 
         def rotate(self, val):
                 '''val between 0 and 255'''
-                pulse = float(val) / (255.0 / self.MAX_WIDTH)
-                if (val > 0 and pulse < self.MIN_WIDTH):
-                        pulse = self.MIN_WIDTH
+                pulse = ratio(val, 0, 255, self.MIN_WIDTH, self.MAX_WIDTH)
                 self.pwm.ChangeDutyCycle(pulse)
 
 def main():
