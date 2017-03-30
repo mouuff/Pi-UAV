ebef0cd1cf8b2e8a05c704b563222588e123f88d
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-03-30 08:51:14 +0200

    .3

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ def main():
         server = Server()
         while (True):
                 data = server.recv()
+                # socket.timeout
                 controler.control(*data)
                 print(data)
 
